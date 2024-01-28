0b66165d68c054402e332421fa5081b827b4fc73
PushpenderDangi <pushpender0006@gmail.com>
2024-01-28 15:02:40 +0530

    Updates

---
+++ precache-manifest.aa4deec5e9a03a0650dbdccfce96e5ca.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0004c3d29f7afa879681e9d5fe6625af",
+    "url": "/PushpenderDangi/Website.git/index.html"
+  },
+  {
+    "revision": "b6d123a6247aabc90e09",
+    "url": "/PushpenderDangi/Website.git/static/css/main.d2ebc2c6.chunk.css"
+  },
+  {
+    "revision": "24b81e70c5bf41570083",
+    "url": "/PushpenderDangi/Website.git/static/js/2.e8d94b14.chunk.js"
+  },
+  {
+    "revision": "ad62b22e4c48a23850d8",
+    "url": "/PushpenderDangi/Website.git/static/js/3.17f431cc.chunk.js"
+  },
+  {
+    "revision": "b6d123a6247aabc90e09",
+    "url": "/PushpenderDangi/Website.git/static/js/main.b0c3a419.chunk.js"
+  },
+  {
+    "revision": "e381312e71d578823ab8",
+    "url": "/PushpenderDangi/Website.git/static/js/runtime~main.c7480ab9.js"
+  },
+  {
+    "revision": "76449feeec22be2ba8dcecba021fd34f",
+    "url": "/PushpenderDangi/Website.git/static/media/Linkedin.76449fee.png"
+  },
+  {
+    "revision": "1033a29bae3329eb3cdac21c7514339b",
+    "url": "/PushpenderDangi/Website.git/static/media/Verified.1033a29b.mp4"
+  },
+  {
+    "revision": "20682936d93b6d4cb9fd395c9857b30b",
+    "url": "/PushpenderDangi/Website.git/static/media/architect.20682936.jpg"
+  },
+  {
+    "revision": "7a313f5743cd1de5d5dfa390ff6d6a18",
+    "url": "/PushpenderDangi/Website.git/static/media/asset2.7a313f57.jpg"
+  },
+  {
+    "revision": "53436696e4abbe7d6d0c578c22ee970e",
+    "url": "/PushpenderDangi/Website.git/static/media/background.53436696.jpg"
+  },
+  {
+    "revision": "a40f3c94dc03919d5425c70fc66cd650",
+    "url": "/PushpenderDangi/Website.git/static/media/cloth1.a40f3c94.jpg"
+  },
+  {
+    "revision": "a73b165aa1869f296710c9fcb251216f",
+    "url": "/PushpenderDangi/Website.git/static/media/contactus.a73b165a.png"
+  },
+  {
+    "revision": "06250ab13da4ff127e74c099ee8dee57",
+    "url": "/PushpenderDangi/Website.git/static/media/fb.06250ab1.png"
+  },
+  {
+    "revision": "7bfebc6d84805f84d1b177b140788a39",
+    "url": "/PushpenderDangi/Website.git/static/media/keenable.7bfebc6d.png"
+  },
+  {
+    "revision": "52a15eda025b4cde8f9843975d4f957a",
+    "url": "/PushpenderDangi/Website.git/static/media/monitoring.52a15eda.jpg"
+  },
+  {
+    "revision": "3c1aa2b9a08b8fd9a3ea3423812229b0",
+    "url": "/PushpenderDangi/Website.git/static/media/more-about.3c1aa2b9.png"
+  },
+  {
+    "revision": "cec9009aa29a5f46ee1d04d0969ffa3b",
+    "url": "/PushpenderDangi/Website.git/static/media/partner.cec9009a.png"
+  },
+  {
+    "revision": "8a20aca12d7f32441ee79a317e7c7ce8",
+    "url": "/PushpenderDangi/Website.git/static/media/plant1.8a20aca1.jpg"
+  },
+  {
+    "revision": "8f0b063cd86010c9a321a287e2ed8770",
+    "url": "/PushpenderDangi/Website.git/static/media/software.8f0b063c.jpg"
+  }
+]);
